575c6bbd1aae774601b9532797c0d2a00b90c13c
AjaySaini9582 <ajsaini9582@gmail.com>
2024-02-27 22:54:21 +0530

    adding  two sum file

--- src/com/company/array/TwoSumProblem.java
+++ src/com/company/array/TwoSumProblem.java
@@ -1,8 +1,29 @@
 package com.company.array;
 
+import java.util.HashMap;
+
 public class TwoSumProblem {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (!map.containsKey(diff)) {
+                map.put(nums[i], i);
+            } else {
+                ans[0] = map.get(diff);
+                ans[1] = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        int[] nums ={2,7,11,15};
+        int target=9;
+        int []an=TwoSumProblem.twoSum(nums,9);
+        System.out.println(an[0]+" "+an[1]);
     }
 }
